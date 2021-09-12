802f3427cb883fd4d33e331ad8783382bdae62d9
Caleb Wagner <calebwagner5@gmail.com>
2021-09-12 07:03:55 -0500

    changed local storage to different token name

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { ApplicationViews } from "./ApplicationViews";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+
+export const App = () => (
+  <>
+    <Route
+      render={() => {
+        if (localStorage.getItem("uteachilearn_user")) {
+          return (
+            <>
+              <ApplicationViews />
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
+);
