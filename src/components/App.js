85e543f7686181b30abc1d405e5e97294721f5cf
Caleb Wagner <calebwagner5@gmail.com>
2021-09-12 20:30:27 -0500

    styling components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { ApplicationViews } from "./ApplicationViews";
+import { Aside } from "./aside/Aside";
 import { Login } from "./auth/Login";
 import { Register } from "./auth/Register";
+import { SearchBar } from "./posts/SearchBar";
 
 export const App = () => (
   <>
@@ -11,7 +13,15 @@ export const App = () => (
         if (localStorage.getItem("uteachilearn_token")) {
           return (
             <>
-              <ApplicationViews />
+              <div class="col-span-3 sticky top-0 z-50">
+                <SearchBar />
+              </div>
+              <aside class="col-span-3 sticky top-0 z-50">
+                <Aside />
+              </aside>
+              <main class="col-span-8">
+                <ApplicationViews />
+              </main>
             </>
           );
         } else {
--- src/components/posts/PostDetail.js
+++ src/components/posts/PostDetail.js
@@ -1,43 +1,61 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { PostContext } from "./PostProvider";
+import "tailwindcss/tailwind.css";
+import "./css/PostDetail.css";
 
 export const PostDetail = ({ post }) => {
   const { posts, getPosts, deletePost } = useContext(PostContext);
 
   return (
-    <section className="post">
-      <div>
-        <Link to={`/posts/${post.id}`}>
-          <img
-            className="profile_image"
-            //   src={post.user.image_url}
-            //   src={require("./images/profilepic.jpg")}
-            src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
-            alt="profile picture"
-          ></img>
-        </Link>
-      </div>{" "}
-      <h3 className="post__title">
-        <Link to={`/edit/${post.id}`}>{post.title}</Link>
-      </h3>
-      <div className="post__author">
-        Author: {post.user.user.first_name} {post.user.user.last_name}
+    <section className="p-8 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <div className="md:flex-shrink-0">
+        <div className="flex">
+          <Link to={`/posts/${post.id}`}>
+            <img
+              className="h-48 w-16 object-scale-down	 md:w-48"
+              //   src={post.user.image_url}
+              //   src={require("./images/profilepic.jpg")}
+              src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+              alt="profile picture"
+            ></img>
+          </Link>
+          <div className="space-y-4">
+            <h3 className="">
+              <Link to={`/edit/${post.id}`}>
+                Title: {post.title} ||{" "}
+                <div className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                  {post.category?.title}
+                </div>
+              </Link>
+            </h3>
+            <div className="inline-">
+              Author: {post.user.user.first_name} {post.user.user.last_name}
+            </div>
+            <div>Date: {post.created_on}</div>
+          </div>
+        </div>
+        <div>
+          <h5>Description: {post.description}</h5>
+        </div>
+        {/* <img
+          className="h-48 w-full object-cover md:w-48"
+          src="https://www.aihr.com/wp-content/uploads/Learning-and-development.png"
+          alt="picture"
+        /> */}
+        {/* <Link to={`/posts/${post.id}`}>Comments</Link> */}
+        <div className="flex items-center justify-center">
+          <button className="m-8 py-2 px-4 bg-blue-700 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
+            Save
+          </button>
+          <button
+            className="py-2 px-4 bg-red-700 text-white font-semibold rounded-lg shadow-md hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75"
+            onClick={() => deletePost(post.id)}
+          >
+            Delete
+          </button>
+        </div>
       </div>
-      <div>
-        <h3>Description:</h3>
-        <h5>{post.description}</h5>
-      </div>
-      <img
-        className="post_image"
-        src="https://www.aihr.com/wp-content/uploads/Learning-and-development.png"
-        alt="picture"
-      ></img>
-      <div className="post__category">Category: {post.category?.title}</div>
-      {/* <Link to={`/posts/${post.id}`}>Comments</Link> */}
-      <button className="btn delete__btn" onClick={() => deletePost(post.id)}>
-        Delete
-      </button>
     </section>
   );
 };
