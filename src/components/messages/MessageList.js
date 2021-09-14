997c30b048d044be399497fd7910302c7457a893
Caleb Wagner <calebwagner5@gmail.com>
2021-09-14 06:10:18 -0500

    sorted messages by user id

--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -1,6 +1,7 @@
-import React, { useContext, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
 import "tailwindcss/tailwind.css";
+import { UserContext } from "../users/UserProviders";
 import { MessageDetail } from "./MessageDetail";
 import { MessageForm } from "./MessageForm";
 import { MessageContext } from "./MessageProvider";
@@ -8,16 +9,22 @@ import { MessageContext } from "./MessageProvider";
 export const MessageList = () => {
   const { messages, getMessages } = useContext(MessageContext);
 
+  const currentUserId = parseInt(localStorage.getItem("uteachilearn_token"));
+
   useEffect(() => {
     getMessages();
   }, []);
 
+  const filteredMsgByUserId = messages.filter(
+    (message) => currentUserId === message.recipient.user.id
+  );
+
   return (
     <section className="space-y-6 pb-8">
-      <div>
+      <div className="float-right w-1/4 sticky top-0 z-50 col-span-3 ">
         <MessageForm />
       </div>
-      {messages.map((message) => {
+      {filteredMsgByUserId.map((message) => {
         return <MessageDetail key={message.id} message={message} />;
       })}
     </section>
