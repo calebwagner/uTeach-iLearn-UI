67c14960d87e4db58d887ceb293b245ad94254f0
Caleb Wagner <calebwagner5@gmail.com>
2021-09-14 07:54:31 -0500

    aded links to different pages

--- src/components/aside/Aside.js
+++ src/components/aside/Aside.js
@@ -8,16 +8,16 @@ export const Aside = () => {
     <section className="space-y-6 pb-8 sticky top-0 z-50 bg-white shadow w-64 my-2 m-8">
       <div>
         <div className="block p-4 text-grey-darker font-bold border-purple hover:bg-grey-lighter border-r-4">
-          <Link>uTeachiLearn</Link>
+          <Link to={`/`}>uTeachiLearn</Link>
         </div>
         <div className="block p-4 text-grey-darker font-bold border-purple hover:bg-grey-lighter border-r-4">
-          <Link>Categories</Link>
+          <Link to={`/categories`}>Categories</Link>
         </div>
         <div className="block p-4 text-grey-darker font-bold border-purple hover:bg-grey-lighter border-r-4">
           <Link to={`/messages`}>Messages</Link>
         </div>
         <div className="block p-4 text-grey-darker font-bold border-purple hover:bg-grey-lighter border-r-4">
-          <Link>Profile</Link>
+          <Link to={`/profile`}>Profile</Link>
         </div>
       </div>
     </section>
