8b4042e1adb228ab2bb5dbb870753d81e33a0e77
Caleb Wagner <calebwagner5@gmail.com>
2021-09-16 07:37:59 -0500

    added user meeting info to display

--- src/components/events/MeetingDetail.js
+++ src/components/events/MeetingDetail.js
@@ -7,8 +7,13 @@ export const Meetings = ({ meeting }) => {
   return (
     <section className="m-8category p-8 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       <div className="md:flex-shrink-0 ">
-        <h1>Meeting with: {meeting.connection.user.id}</h1>
+        <h1>Connection ID: {meeting.connection.user.id}</h1>
+        <h1>
+          Meeting with: {meeting.connection.user.first_name}
+          {meeting.connection.user.last_name}
+        </h1>
         <div>Description: {meeting.description}</div>
+        <div>Date: {meeting.scheduled_date}</div>
         <button
           className="py-2 px-4 bg-red-700 text-white font-semibold rounded-lg shadow-md hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75"
           onClick={() => deleteMeeting(meeting.id)}
